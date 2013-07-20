0c08820413eade47ac4686a3798d259fbf97f421
lukaserat <joseph.lacerna@gmail.com>
2013-07-21 00:41:03 +0800

    a js that will load the high res hidden and replace thec corresponding low res image when loaded

---
+++ pre-loading-images.js
@@ -0,0 +1,18 @@
+var img = jQuery('<img>').css({
+    position: 'absolute',
+    left: '-9999px'
+});
+  
+jQuery('img[data-src]').each(function () {
+    var self = jQuery(this),
+        highres_url = self.data('src'),
+        preloader = img.clone()
+                       .attr('src', highres_url)
+    ;
+
+    preloader.on('load', function () {
+        // should be 'cached' by now
+        self.attr('src', highres_url);
+    }).appendTo('body');
+
+});
